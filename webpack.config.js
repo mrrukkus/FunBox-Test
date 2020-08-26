46cdd131ef5c9c7afae43b4da3a2d183409ec39b
mrrukkus <46551997+mrrukkus@users.noreply.github.com>
2020-08-26 22:12:05 +0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.tsx`,
+  output: {
+    filename: `bundle.js`,
+    publicPath: `/`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: true,
+    inline: true,
+    port: 1337,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      },
+      {
+        test: /\.(tsx|ts)?$/,
+        loader: `ts-loader`
+      }
+    ],
+  },
+  resolve: {
+    extensions: [`.ts`, `.tsx`, `.js`, `json`]
+  },
+  devtool: `source-map`,
+};
